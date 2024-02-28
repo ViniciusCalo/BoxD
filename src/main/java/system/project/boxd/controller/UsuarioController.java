2899b7f547b106300ab3754c7ae3df3893cc74ff
ViniciusCalo <vini.stcal@gmail.com>
2024-02-27 22:56:30 -0300

    Adding a Web Config to enable safe access to the URLs

--- src/main/java/system/project/boxd/controller/UsuarioController.java
+++ src/main/java/system/project/boxd/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package system.project.boxd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import system.project.boxd.dao.IUsuario;
 import system.project.boxd.model.Usuario;
 
+import java.net.PasswordAuthentication;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +16,10 @@ import java.util.Optional;
 public class UsuarioController {
     @Autowired
     private IUsuario dao;
+    @Autowired
+    private PasswordEncoder encoder;
 
-    @GetMapping
+    @GetMapping("/cadastro")
     public List<Usuario> listaUsuario(){
         return (List<Usuario>) dao.findAll();
     }
